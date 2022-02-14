56727b2d4ec899acdf3eac616f0abd7e20257f46
LucasBitt7 <78456901+LucasBitt7@users.noreply.github.com>
2022-02-14 16:54:23 -0300

    Update delegationEncodeFunction.sol

--- delegationEncodeFunction.sol
+++ delegationEncodeFunction.sol
@@ -7,3 +7,11 @@ contract Encode {
 }
 
 ////encode some function for use on delegation call
+////Usage of delegatecall is particularly risky and has been used as an attack vector on multiple historic hacks. With it, 
+////your contract is practically saying "here, -other contract- or -other library-, do whatever you want with my state".
+////Delegates have complete access to your contract's state. The delegatecall function is a powerful feature, but a dangerous one,
+////and must be used with extreme care.
+
+////Please refer to the The Parity Wallet Hack Explained article for an accurate explanation of how this idea was used to steal 30M USD.
+
+
